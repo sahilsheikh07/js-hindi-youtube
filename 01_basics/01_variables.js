6227840f9f96c0da60a520eba963853bc9638ec7
sahilsheikh07 <143924748+sahilsheikh07@users.noreply.github.com>
2023-09-03 07:59:18 +0000

    i learn data typs

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 146634
+let accountEmail ="sahil@gmail.com"
+var accountPassword = "12345"
+accoutCity = "udaipur"
+
+// accountId = 3
+
+accountEmail = "ss@sahil.com"
+accountPassword = "1433"
+accoutCity = "rajasthyan"
+
+console.log(accountId);
+console.table( [ accountId,accountEmail,accoutCity,accountPassword])
