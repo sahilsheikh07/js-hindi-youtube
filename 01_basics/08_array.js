dcacbcde1d92889b3c03b9c74599132eec2532f2
sahilsheikh07 <143924748+sahilsheikh07@users.noreply.github.com>
2023-09-10 04:48:22 +0000

    arry's

---
+++ 01_basics/08_array.js
@@ -0,0 +1,43 @@
+const myArr = [1,2,3,4,5,6]
+
+const myHeros = ["hulk","iron man"]
+
+const myArr2 = new Array (1,2,3,34)
+
+// console.log(myArr [0])
+// console.log(myArr2[4]);
+// console.log(myHeros[0])
+
+// Array method
+// myArr.push(5)
+// console.log(myArr);
+// myHeros.push(3)
+// myHeros.pop()
+// console.log(myHeros);
+// console.log(myHeros);
+// myArr.unshift(3)
+// myArr.shift()
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// const newArr = myArr.join()
+// console.log(myArr)
+
+
+// slice splice
+
+console.log("A",myArr);
+
+const myn1 =myArr.slice(2,4)
+
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2); 
+
+console.log("C",myArr);
+
+
+
+
