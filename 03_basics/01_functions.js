140318e98c260ef1f993649ea127a966f07df6bc
sahilsheikh07 <143924748+sahilsheikh07@users.noreply.github.com>
2023-09-11 06:55:52 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,34 @@
+function myname (){
+    // console.log("s");
+    // console.log("a");
+    // console.log("h");
+    // console.log("i");
+    // console.log("l");
+}
+
+myname()
+
+function addTowNumbers(number1 , number2){
+
+// console.log(number1 + number2);
+}
+addTowNumbers( 4,6)
+
+function addTowNumbers(number1,number2){
+    let result = number1 + number2
+    return result
+    }
+   const saif = addTowNumbers(4,6)
+   //console.log("Result:",saif)
+    
+
+
+function loginSahilMassage(username){
+    if (username === undefined) {
+        console.log("please enter a username");
+        
+    }
+return `${username} just loggedin`
+}
+
+console.log(loginSahilMassage());
