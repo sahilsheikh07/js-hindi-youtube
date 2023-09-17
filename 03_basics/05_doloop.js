011b5dd666ad37c0eb90f8ca03d54ea9e34637b7
sahilsheikh07 <143924748+sahilsheikh07@users.noreply.github.com>
2023-09-17 07:30:59 +0000

    do loop

---
+++ 03_basics/05_doloop.js
@@ -0,0 +1,20 @@
+
+
+let index = 0
+while (index <=20) {
+    //console.log(`value of index is ${index}`);
+    index = index + 4 
+}
+
+let myArray = ["rajesh","rabbani","rakhesh"]
+let arr = 0
+while( arr < myArray.length){
+    //console.log(`value of is ${myArray [arr]}`);
+    arr = arr + 1
+}
+
+let score = 15
+do {
+    console.log(`score is ${score}`);
+    score++
+} while ( score <= 10);
